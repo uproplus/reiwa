9d9f658aff4a09d67bcfdcaa9d9af867f1e36a11
reiwa <reiwa@outlook.com.vn>
2019-05-09 23:11:17 +0700

    refactor

---
+++ lib/stateless_widgets/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_web/material.dart';
+import 'package:reiwa/stateless_widgets/my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
